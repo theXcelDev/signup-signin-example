57a4ff711f3dac7e899678495c98b256f654e6b2
Excellence DevSolutions <excellence.devsolutions@gmail.com>
2024-11-07 13:25:33 -0300

    Designing Home page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,15 @@
 const App = () => {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <header className="header">
+        <h1 className="my-app-title">myApp</h1>
+        <button className="sign-out-action">Sign out</button>
+      </header>
+      <main className="container">
+        <h2 className="welcome-text">Welcome to myApp!</h2>
+      </main>
+    </>
+  )
 }
 
 export default App
